1198910584a4600324d8a9d4938a104f9d163ec9
Rishab Raj <rr0948555@gmail.com>
2025-12-11 13:53:26 +0530

    ComponentDidMount

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ComponentDidMount from './ComponentDidMount'
+
+const App = () => {
+  return (
+    <div> 
+       <h2>This is App.jsx method</h2>
+       <ComponentDidMount/>
+    </div>
+  )
+}
+
+export default App
